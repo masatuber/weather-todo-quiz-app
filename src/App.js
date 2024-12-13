bb057c1c9039f236f53b8fd3ee98f47b7824241c
masattuber <uvervivid1231@icloud.com>
2024-12-13 18:31:06 +0900

    変更1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-//App.js
-import Title from "./components/Title";   
-import  Form from "./components/Form";  
-import Results from "./components/Results";  
-import Loading from "./components/Loading";
-import { Link, } from 'react-router-dom'; //ルーティング設定（リンク）
+import './App.css';
+import { BrowserRouter, Route, Routes, Link,  } from 'react-router-dom';
+import Home from './components/home';  //アプリHomeのページ追加
+import NotFound from './components/not_found'; //ルーティング設定で見つからない時のページ
+import Title from "./components/Title";   //天気インポート
+import  Form from "./components/Form"; //天気インポート
+import Results from "./components/Results";  //天気インポート
+import Loading from "./components/Loading"; //天気インポート
 import { useState } from "react";
 import axios from "axios";
-import Digit from "./DigitalDateTime"; //日時追加
-import './App.css';
-
-
-
+import Digit from "./components/DigitalDateTime"; //リアルタイム時計
+import TodoApps from './components/TodoApps';  //TodoAppsのページ追加
+import Inquiry from './components/Inquiry';  //問い合わせページ追加
+import CalendarApp from './components/CalendarApp';
 function App() {
-   
-  const [city, setCity] = useState("");
+
+ const [city, setCity] = useState("");
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState({
       country: "",
@@ -38,32 +39,46 @@ function App() {
         setLoading(false);
     })
     .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"));
-    
-    
+        
 }
 //コンポーネント配置
 //リンク追加
-return (
-<div className="wrapper">
-        <div className="container">
-            <Title />
-            <Form getWeather={getWeather} setCity={setCity} city={city} />
-            {loading ? <Loading /> : <Results results={results} />}
-            <div className="link">
-            <menu>
-                <li><Link to="https://www.youtube.com/@uverworldroyz1231/about"><font color="black">YouTubeチャンネルはこちら</font></Link></li>
-                <li><Link to="https://quiz-app2-masatuber.netlify.app"><font color="black">クイズアプリはこちら</font></Link></li>
-                <li><Link to="https://todoapp-01-time-01.netlify.app/"><font color="black">タスク管理アプリはこちら</font></Link></li>
-            </menu>
-            </div>
-            <button class="btn"><Link to="msnweather:"><font color="black">Windows天気アプリ起動</font></Link></button>
-            <button class="btn"><Link to="https://www.google.com/maps/d/viewer?mid=1VFroURwY3NC19qYVgHeMpb_c2vM&hl=ja&ll=-3.81666561775622e-14%2C17.82600400000001&z=1"> <font color="black">世界地図</font></Link>
-            </button>
-            <div className="dit"><Digit /></div>
-            
+  return (
+    <>
+          <div className="wrapper">
+            <div className="container">
+                <Title />
+                <Form getWeather={getWeather} setCity={setCity} city={city} />
+                {loading ? <Loading /> : <Results results={results} />}
+            <font color="black"><div className="dit"><Digit /></div></font>      
+          <div className="link">
+      <BrowserRouter>
+        <ol>
+          <li><Link to="/">World Weather アプリ</Link></li>
+          <li><Link to="/src/components/TodoApps.js" >ReactTodoアプリを開く</Link></li>
+          <li><Link to="/src/components/CalendarApp.js">簡易カレンダーアプリを開く</Link></li>
+          <li><Link to="https://quiz-app2-masatuber.netlify.app" target="_blank" rel="noopener noreferrer"><font color="black">クイズアプリを開く</font></Link></li>
+          <li><Link to="https://www.youtube.com/@uverworldroyz1231/about" target="_blank" rel="noopener noreferrer">Youtubeチャンネルはこちら</Link></li>
+          <li><Link to="/src/components/Inquiry.js">開発者にお問い合わせページはこちら</Link></li>
+          
+        </ol>
+      <button><Link to="msnweather:"><font color="black">Windows天気アプリ起動</font></Link></button>
+      <button><Link to="https://www.google.com/maps/d/viewer?mid=1VFroURwY3NC19qYVgHeMpb_c2vM&hl=ja&ll=-3.81666561775622e-14%2C17.82600400000001&z=1" target="_blank" rel="noopener noreferrer"> <font color="black">世界地図</font></Link>
+      </button>
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/src/components/TodoApps.js" element={<TodoApps />} />
+          <Route path="/src/components/inquiry.js" element={<Inquiry />} />
+          <Route path="/src/components/CalendarApp.js" element={<CalendarApp />} /> 
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+          </div>
         </div>
-</div>
- )
-};
+      </div>
+    </>
+  );
+}
+
 
 export default App;
