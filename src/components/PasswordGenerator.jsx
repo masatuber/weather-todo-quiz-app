def22f1980f5fe0ffc23989ac00c513c25e29675
masattuber <uvervivid1231@icloud.com>
2025-01-19 10:44:36 +0900

    “PWコピーボタン追加”

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,22 +1,57 @@
-//PasswordGenerator.js
+// PasswordGenerator.js
+import { useState } from "react";
 import randomize from "randomatic";
-import { useState, }  from "react";
- const  PasswordGenerator= () => {
-   const [ password, setPassword ] = useState( '' );
-   //パスワード生成ハンドラー
-   const generatePassword = ( ) => {
-    const generatedPassword = randomize('Aa0', 12);
+import { Button, Snackbar } from "@mui/material";
+
+const PasswordGenerator = () => {
+  const [password, setPassword] = useState("");
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  // パスワード生成ハンドラー
+  const generatePassword = () => {
+    const generatedPassword = randomize("Aa0", 12);
     setPassword(generatedPassword);
   };
 
+  // クリップボードにコピー
+  const handleCopyClick = () => {
+    if (password) {
+      navigator.clipboard
+        .writeText(password)
+        .then(() => setCopySuccess(true))
+        .catch((err) => console.error("コピーに失敗しました:", err));
+    }
+  };
+
+  const handleCloseSnackbar = () => setCopySuccess(false);
+
   return (
     <>
-    <h2><b>パスワードジェネレータ（12桁・大文字・小文字・数字含む）</b></h2>
-        <h2>ランダムパスワード生成</h2>
-             <button onClick={generatePassword} className="passwordButton"> パスワード生成12桁</button><br/>
-        <div className="pwMain">
-        <h4>生成されたパスワード：  {password}</h4>
-        </div>
+      <h2>
+        <b>パスワードジェネレータ（12桁・大文字・小文字・数字含む）</b>
+      </h2>
+      <h2>ランダムパスワード生成</h2>
+      <button onClick={generatePassword} className="passwordButton">
+        パスワード生成12桁
+      </button>
+      <br />
+      <div className="pwMain">
+        <h4>生成されたパスワード： {password}</h4>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleCopyClick}
+          disabled={!password} // パスワード未生成時にボタンを無効化
+        >
+          パスワードをコピーする
+        </Button>
+        <Snackbar
+          open={copySuccess}
+          autoHideDuration={2000}
+          onClose={handleCloseSnackbar}
+          message="パスワードをコピーしました。"
+        />
+      </div>
     </>
   );
 };
