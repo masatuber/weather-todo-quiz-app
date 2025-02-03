83cc34283688da5653fda988c0d8bc577c42c1ba
masattuber <uvervivid1231@icloud.com>
2025-02-03 16:59:38 +0900

    “追加1”

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    // コード分割
-    chunkSizeWarningLimit: 1600,
-    // ツリーシェイキング
-    minify: 'esbuild', // esbuild による minify を推奨
-    // ライブラリの外部化
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-    },
-  },
-} );
+  
+} )
